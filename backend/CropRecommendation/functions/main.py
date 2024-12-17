609f522966fce4af6a60f7b0efd3571f04f00a68
ShreyashRahinj <shreyashrahinj1@gmail.com>
2024-12-17 23:31:56 +0530

    firebase functions done

--- backend/CropRecommendation/functions/main.py
+++ backend/CropRecommendation/functions/main.py
@@ -1,7 +1,3 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn,options
 from firebase_admin import initialize_app
 import pickle
@@ -9,6 +5,10 @@ import json
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 initialize_app()
 
@@ -18,7 +18,7 @@ initialize_app()
         cors_methods=["get", "post"],
     )
 )
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
+def on_request_recommendation(req: https_fn.Request) -> https_fn.Response:
 
     req = json.loads(req.data)
 
@@ -28,8 +28,8 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
     ph = req['ph']
     rainfall = req['rainfall']
 
-    model = pickle.load(open('rfc_model.pkl','rb'))
-    ms = pickle.load(open('minmaxscaler.pkl','rb'))
+    model = pickle.load(open('models/Recommendation/rfc_model.pkl','rb'))
+    ms = pickle.load(open('models/Recommendation/minmaxscaler.pkl','rb'))
 
     features = np.array([[N,temperature,humidity,ph,rainfall]])
     transformed_features = ms.fit_transform(features)
@@ -38,3 +38,46 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange", 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana", 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans", 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
     return https_fn.Response(crop_dict[prediction[0]])
+
+@https_fn.on_request(
+    cors=options.CorsOptions(
+        cors_origins=[r"firebase\.com$", r"https://flutter\.com"],
+        cors_methods=["get", "post"],
+    )
+)
+def on_request_prediction(req: https_fn.Request) -> https_fn.Response:
+
+    req = json.loads(req.data)
+
+    crop = req['crop']
+
+    model = pickle.load(open(f'models/PricePrediction/{crop}.pkl','rb'))
+
+    forecast_sarima = model.get_forecast(steps=12)
+    forecast_sarima_mean = forecast_sarima.predicted_mean
+    
+    return forecast_sarima_mean.to_json(orient='records')
+
+@https_fn.on_request()
+def on_request_schemes(req: https_fn.Request) -> https_fn.Response:
+    url = "https://agriwelfare.gov.in/en/Major"
+
+    data = requests.get(url,verify=False)
+    soup = BeautifulSoup(data.content,'html.parser')
+
+    table_body = soup.find('tbody')
+    rows = table_body.find_all('tr')
+
+    scheme_dict = {}
+    
+
+    for row in rows:
+        title = row.find_all('td')[1].text
+        site_url = row.find_all('td')[-1].find_all('a')[-1]['href']
+
+        if ".pdf" not in site_url:
+            scheme_dict[title] = site_url
+
+    df = pd.DataFrame(scheme_dict.items(),columns=['Title','Link'])
+    
+    return df.to_json(orient='records')
